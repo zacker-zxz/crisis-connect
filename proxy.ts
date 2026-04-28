f17946c052edc7e4240e7c3cfd9c39cd29a125c5
Tejas <tejastayade4@gmail.com>
2026-04-28 22:49:11 +0530

    update

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/request';
+import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
 
 // These routes require authentication
